0300d6cb618bb18e7d56aa30d88dc90114c3b9b8
Iratsareva <irinatsarea1211@gmail.com>
2024-11-22 14:30:03 +0300

    update 1 Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,10 @@
 class Main{
     public static void main(String[] args) {
         System.out.print("Это добавлено в ветке main");
+
+        System.out.println();
+        for(int i=0; i<6;i++){
+            System.out.println(i);
+        }
     }
 }
